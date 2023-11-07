e3081deff17a6dd5b50a63a05dcef89cd8afa45f
Jacob Cunitz <cunitz.jacob@gmail.com>
2023-11-07 17:51:44 -0600

    successfully connecting order and legacy db, added documentation

--- connect.js
+++ connect.js
@@ -1,11 +1,19 @@
-//document.body.innerHTML = '<h1> Please work </h1>';
+//connect.js
+//connects to legacy db and is able to take values from it and enter them into the self created order db
+
 
 const express = require('express');
 const mysql = require('mysql');
+const sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
  
 const app = express();
 const PORT = 3000;
- 
+
+const parts = [];             //holds part ids
+
+//-------------------------------------------------------------------------------
+
 // Create a connection to the database
 const connection = mysql.createConnection({
   host: 'blitz.cs.niu.edu',
@@ -22,14 +30,73 @@ connection.connect(error => {
         throw error;
     }
 
-    connection.query("SELECT * FROM parts",  function (err, result, fields) {
+    //get all numbers from parts
+    connection.query("SELECT number FROM parts",  function (err, result, fields) {
         if (err) throw err;
-        console.log(result); 
+        for (var i = 0; i < result.length; i++) {
+            var row = result[i];
+            parts[i] = row.number;                    //put into parts array
+        }
+
+  //      print(parts);
+        console.log("job complete, exit database connection");
+        connection.end();                             //close connection
+        insertOrder(parts, orderdb);
+
     });
-     
+
     //If Everything goes correct, Then start Express Server
     app.listen(PORT, ()=>{
         console.log("Database connection is Ready and "
              + "Server is Listening on Port ", PORT);
     })
 });
+
+//-----------------------------------------------------------------------------------
+//order db connection
+var dbpath = './order.db';
+var dbExists = fs.existsSync(dbpath);
+if (!dbExists) {
+    fs.openSync(dbpath, 'w');
+}
+
+//connect to order db
+var orderdb = new sqlite3.Database(dbpath, sqlite3.OPEN_READWRITE,  (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+
+    console.log("successfuly connected to order database");
+    });
+
+//BEGGINGING OF FUNCTIONS---------------------------------------------------------------------------------------------------------------
+
+//prints the parts array
+function print(parts) {
+    for (var i = 0; i < parts.length; i++) {
+        console.log(parts[i] + "print");
+    }
+}
+
+//takes all of the parts array and inserts them into the order db
+function insertOrder(parts, db) {
+
+ //   const ins = db.prepare('INSERT INTO OrderItem (partId) VALUES (?)');
+
+    for (var i = 0; i < parts.length; i++) {
+        db.run('INSERT INTO OrderItem (partId) VALUES (?)',parts[i], function(err) {     //insert part id
+            if (err) {
+              return console.log(err.message);
+            }
+        });
+    }
+
+    runQueries(db);
+}
+
+//function to run queries on the order db
+function runQueries(db) {
+    db.each('SELECT partId FROM OrderItem', (err, row) => {
+        console.log(`${row.partId}`)
+    })
+}
--- createDB.js
+++ createDB.js
@@ -1,31 +1,24 @@
-var sqlite3 = require('sqlite3').verbose();
-const express = require('express')
-const app = express()
-var port = process.env.PORT || 3000;
+//createDB.js
+//creates the order db
 
+var sqlite3 = require('sqlite3').verbose();
 
 var db = new sqlite3.Database('./order.db');
 createTables(db);
 runQueries(db);
 
-
+//creates all of the tables for the order database will need to be extended later
 function createTables(newdb) {
     newdb.serialize(() => {
         newdb.run('CREATE TABLE CustomerOrder (orderId INTEGER PRIMARY KEY, customerId INTEGER)');
         newdb.run('CREATE TABLE OrderItem (partId INTEGER PRIMARY KEY)');
-
-    const data = [
-        [1, 2]
-    ];
-
-    const ins = newdb.prepare('INSERT INTO CustomerOrder (orderId, customerId) VALUES (?, ?)');
-
-    data.forEach((row) => ins.run(row));
-    ins.finalize();   
 });
 
 }
 
+//this is used to run queries on the customerorder table
+//keeping it here for now but will probably not be needed
+//as the purpose of this files is to just create the tables in the order database
 function runQueries(newdb) {
     newdb.each('SELECT orderId, customerId FROM CustomerOrder', (err, row) => {
         console.log(`${row.orderId}: ${row.customerId}`)
